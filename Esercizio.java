0cf3152c3de46100da7fad95582a91a57682f598
Antonio-Milella <antonio.milella@marconibari.edu.it>
2025-02-25 18:44:52 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,117 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Random;
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+public class Esercizio {
+    private static Random r = new Random();
+    private static Scanner in = new Scanner(System.in);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+    public static void main(String[] args) {
+        int m, n, a, i, j, grossezza, ora;
+        
+        do {
+            System.out.println("Inserisci il numero di animali che arrivano al lago.");
+            a = Integer.ParseInt(in.next());
+        }
+        while (a <= 0);
+        int[] pescato = new int[a * 10];
+        int[] orario = new int[a * 10];
+        
+        n = 0;
+        m = 0;
+        i = 0;
+        while (i < a) {
+            grossezza = (r.nextInt(11) + 1) * 50;
+            ora = (i + 10) % 24;
+            System.out.println("Sta arrivando un animale grosso " + grossezza + " cm");
+            n = inserisciElemento(pescato, n, grossezza, n);
+            m = inserisciElemento(orario, m, ora, m);
+            j = 0;
+            while (j < n) {
+                if (grossezza > pescato[j]) {
+                    n = eliminaElemento(pescato, n, j);
+                    m = eliminaElemento(orario, m, j);
+                } else {
+                    j = j + 1;
+                }
+            }
+            i = i + 1;
+            visualizzaVettore(pescato, orario, n);
+        }
+        System.out.println("Ecco le tipologie di grossezze dei pesci pescati: ");
+        n = rimuoviDuplicati(pescato, n);
+        visualizzaVettore(pescato, orario, n);
+        System.out.println("Il pesce più grosso è di grandezza " + pescato[ricercaMassimo(pescato, n)]);
+        System.out.println("Programma terminato.");
+    }
+    
+    public static int eliminaElemento(int[] v, int n, int ie) {
+        int i, n2;
+        
+        n2 = n - 1;
+        for (i = ie; i <= n - 2; i++) {
+            v[i] = v[i + 1];
+        }
+        
+        return n2;
+    }
+    
+    public static int inserisciElemento(int[] v, int n, int e, int ie) {
+        int i, n2;
+        
+        n2 = n + 1;
+        i = n2 - 1;
+        while (i >= ie + 1) {
+            v[i] = v[i - 1];
+            i = i - 1;
+        }
+        v[ie] = e;
+        
+        return n2;
+    }
+    
+    public static int ricercaMassimo(int[] v, int n) {
+        int imax, i;
+        
+        imax = 0;
+        for (i = 0; i <= n - 1; i++) {
+            if (v[imax] > v[i]) {
+            } else {
+                imax = i;
+            }
+        }
+        
+        return imax;
+    }
+    
+    public static int rimuoviDuplicati(int[] v, int n) {
+        int i, j;
+        
+        i = 0;
+        while (i <= n - 2) {
+            j = i + 1;
+            while (j <= n - 1) {
+                if (v[i] == v[j]) {
+                    System.out.println("Elimino " + v[j] + " da cella: " + j);
+                    n = eliminaElemento(v, n, j);
+                } else {
+                    j = j + 1;
+                }
+            }
+            i = i + 1;
+        }
+        
+        return n;
+    }
+    
+    public static void visualizzaVettore(int[] v, int[] w, int n) {
+        int i;
+        
+        if (n == 0) {
+            System.out.println("Vuoto.");
+        } else {
+            for (i = 0; i <= n - 1; i++) {
+                System.out.println("Pesce numero " + i + ": " + v[i] + " | Orario di cattura: " + w[i]);
+            }
+        }
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
